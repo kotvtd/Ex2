5688a68ac858fab919d14c975f3cf8b23e0c741a
Tuandat <>
2026-04-05 03:24:13 +0700

    Done
    Complete Exercise 2

--- Library.js
+++ Library.js
@@ -3,6 +3,8 @@ process.stdin.setEncoding("utf-8");
 
 
 import fs from 'fs'
+import { count } from 'node:console';
+import { parse } from 'node:path';
 import readline from 'node:readline';
 const data = fs.readFileSync('Data.json','utf-8');
 const dataLbr = JSON.parse(data);
@@ -11,23 +13,15 @@ const rl = readline.createInterface({
     output: process.stdout,
 });
 
-
-
-
-
-
-// function SaveJson(){
-//     fs.writeFileSync('Data.json',JSON.stringify(dataLbr, null, 2),'utf-8');
-// }
-
-
+function SaveJson(){
+    fs.writeFileSync('Data.json',JSON.stringify(dataLbr, null, 2),'utf-8');
+}
 // for(let i =0; i < dataLbr.book.length; i++){
 //     if(dataLbr.book[i].id === "B3"){
 //         dataLbr.book.splice(i,1);
 //     }
 // }
 Menu();
-
 
 function CheckBook(){
     let a;
@@ -128,7 +122,6 @@ function CheckBook(){
         }
     });
 }
-
 function CheckHS(){
     let a;
 
@@ -219,7 +212,6 @@ function CheckHS(){
         }
     })
 }
-
 function CheckListMenu(){
     let a;
     console.log("=== Bạn Muốn Tra Cứu Gì ? ====");
@@ -246,12 +238,357 @@ function CheckListMenu(){
         }
     });
 }
-
 function EndLine(){
     console.log("==============================");
     console.log("Bạn hãy nhập lựa chọn phù hợp:");
 }
+function ChooseAdd(){
+    let a;
+    console.log("=== Bạn Muốn Thêm Vào Sách Cũ Hay Thêm Mới? ====");
+    console.log("==============================");
+    console.log("1 => Thêm Cũ.");    
+    console.log("2 => Thêm Mới."); 
+    console.log("0 => Quay lại.");
+    EndLine();
+    rl.question("Bạn muốn làm gì?\n", function(name){
+        a = parseInt(name);
+        switch(a){
+            case 0: 
+                BookHandle();
+                break;
+            case 1:
+                AddOld();
+                break;
+            case 2:
+                AddNew();
+                break;
+            default: 
+                ChooseAdd();
+                break;
+        }
+    })
+}
+function AskAdd(nameAdd, idAdd, totalAdd){
+        rl.question("Bạn có chắc thêm mới: "
+            + "\n===================================================="
+            + "\nTên sách: " + nameAdd
+            + "\nID(Hệ thống quy ước): "
+            + "\nSố lượng: " + totalAdd
+            + "\n===================================================="
+            + "\n1 => Đồng ý."
+            + "\n0 => Không, quay lại."
+            , function(confirm){
+                if(confirm === "1"){
+                    let book = {
+                        "id": "B" + (idAdd + 1).toString(),
+                        "name": nameAdd,
+                        "total": totalAdd,
+                        "count": totalAdd,
+                        "hBorrow": []
+                    };
+                    dataLbr.book.push(book);
+                    SaveJson();
+                    rl.question("Nhấn Enter để quay lại!", function(name){
+                        BookHandle();
+                    });
+                }
+                else if(confirm = "0"){
+                    BookHandle();
+                }
+                else{
+                    AskAdd(nameAdd, idAdd, totalAdd);
+                }
+        });
 
+}
+function AddOld(){
+    let stringName;
+    let found = false;
+    let totalAdd;
+    rl.question("Nhập ID của sách: ", function(inputId){
+        for(let i = 0; i < dataLbr.book.length; i++){
+            if(inputId === dataLbr.book[i].id){
+                found = true;
+                stringName = dataLbr.book[i].name;
+                rl.question("Nhập số lượng thêm vào sách: " + stringName + " ", function(inputNum){
+                    totalAdd = inputNum;
+                    rl.question("Bạn có chắc thêm \n"
+                        + "\n===================================================="
+                        + "\nTên sách: " + stringName
+                        + "\nSố lượng cũ:" + dataLbr.book[i].total
+                        + "\nSố lượng mới: " + (parseInt(totalAdd) + parseInt(dataLbr.book[i].total))
+                        +"\n===================================================="
+                        + "\n1 => Đồng ý."
+                        + "\n0 => Không, quay lại.", function(confirm){ 
+                            if(confirm === "1"){
+                                let temp_tt = parseInt(dataLbr.book[i].total);
+                                let temp_count = parseInt(dataLbr.book[i].count);
+                                dataLbr.book[i].total = (parseInt(totalAdd) + temp_tt).toString();
+                                dataLbr.book[i].count = (parseInt(totalAdd) + temp_count).toString();
+                                SaveJson()
+                                rl.question("Nhấn Enter để quay lại!", function(name){
+                                    ChooseAdd();
+                                });
+                            }
+                            else if(confirm === "0"){
+                                BookHandle();
+                            }
+                            else{
+                                AddOld();
+                            }
+                        });
+                });
+                break;
+            }
+        }
+        if(!found){
+            console.log("Không tồn tại sách có ID: " + inputId + " trong thư viện.")
+                rl.question("Nhấn Enter để quay lại!", function(name){
+                    ChooseAdd();
+                });
+        }
+    });
+}
+function AddNew(){
+    let nameAdd, idAdd, totalAdd ;
+        idAdd = dataLbr.book.length + 1;
+        rl.question("Nhập tên sách: ", function(nameBook){
+            nameAdd = nameBook;
+            rl.question("Nhập số lượng thêm: ", function(inputNum){
+                totalAdd = inputNum;
+                AskAdd(nameAdd, idAdd, totalAdd);
+            });
+        });
+}
+function BookHandle(){
+    let a;
+    console.log("=== Bạn Muốn Thao Tác Gì ? ====");
+    console.log("==============================");
+    console.log("1 => Thêm Sách.");    
+    console.log("0 => Quay lại.");
+    EndLine();
+    rl.question("Bạn muốn làm gì?\n", function(input){
+        a = parseInt(input);
+        switch(a){
+            case 0:
+                Menu();
+                break;
+            case 1:
+                ChooseAdd();
+                break;
+            default:
+                BookHandle();
+                break;
+        }
+    })
+}
+function AddHS(){
+    let a;
+    rl.question("Nhập tên học sinh: ", function(inputName){
+        a = inputName;
+        let temp_idhs = parseInt(dataLbr.hBorrow.length) + 1;
+        let hocsinh = {
+            "id": "H" + temp_idhs.toString(),
+            "name": a,
+            "total": 0,
+            "bBorrow": []
+        };
+
+        rl.question("Bạn có chắc thêm mới: "
+            + "\n===================================================="
+            + "\nTên học sinh: " + a
+            + "\nID(Hệ thống quy ước): " + "H" + temp_idhs.toString()
+            + "\n===================================================="
+            + "\n1 => Đồng ý."
+            + "\n0 => Không, quay lại."
+            , function(confirm){
+                if(confirm === "1"){
+                    dataLbr.hBorrow.push(hocsinh);
+                    SaveJson();
+                    rl.question("Nhấn Enter để quay lại!", function(name){
+                        HSHandle();
+                    });
+                }
+                else{
+                    HSHandle();
+                }
+        });
+    })
+}
+function ReturnBook(){
+    let idTv, idB;
+    let nameTv, nameBook;
+    rl.question("Nhập ID học sinh là thành viên của thư viện: ", function(inputId){
+        idTv = inputId  ;
+        let check = false;
+        for(let i = 0; i < dataLbr.hBorrow.length; i++){
+            if(dataLbr.hBorrow[i].id === idTv){
+                check = true;
+                nameTv = dataLbr.hBorrow[i].name;
+                rl.question("Nhập ID sách trả: ", function(inputIDS){
+                    idB = inputIDS;
+                    for(let j = 0; j < dataLbr.book.length; j++){
+                        if(dataLbr.book[j].id === idB){
+                            check = true;
+                            nameBook = dataLbr.book[j].name;
+                            let tempCountB = parseInt(dataLbr.book[i].count) + 1;
+                            let tempCountH = parseInt(dataLbr.hBorrow[j].total) -1;
+                            //let stringnametemp = dataLbr.book[i].name;
+                            rl.question("Bạn có chắc: "
+                                + "\n========================================="
+                                + "\nHọc sinh: " + nameTv 
+                                + "\Trả sách: " + nameBook
+                                + "\n========================================="
+                                + "\n1 => Đúng vậy."
+                                + "\n0 => Không đúng.", function(confirm){
+                                    let choose = parseInt(confirm);
+                                    switch(choose){
+                                        case 0:
+                                            HSHandle();
+                                            break;
+                                        case 1:
+                                            dataLbr.book[j].count = tempCountB.toString();
+                                            dataLbr.hBorrow[i].total = tempCountH.toString();
+                                            dataLbr.book[j].hBorrow = dataLbr.book[j].hBorrow.filter(function(h){
+                                                return h.id !== idTv;
+                                            });
+                                            dataLbr.hBorrow[i].bBorrow = dataLbr.hBorrow[i].bBorrow.filter(function(b){
+                                                return b.id !== idB;
+                                            });
+                                            SaveJson();
+                                            rl.question("Đã cập nhật danh sách, nhấn Enter để thoát!", function(quit){
+                                                HSHandle();
+                                            });
+                                            break;
+                                        default:
+                                            HSHandle();
+                                            break;
+                                    }
+                                }
+                            );
+                        }
+                    }
+                    if(!check){
+                        check = false;
+                        rl.question("Không có sách này! Nhấn Enter để thoát!!!!", function(confirm){
+                            HSHandle();
+                        })
+                    }
+                })
+            }
+        }
+        if(!check){
+            check = false;
+            rl.question("Không có học viên này! Nhấn Enter để thoát!!!!", function(confirm){
+                HSHandle();
+            })
+        }
+
+    })
+}
+function BorrowBook(){
+    let idTv, idB;
+    let nameTv, nameBook;
+    rl.question("Nhập ID học sinh là thành viên của thư viện: ", function(inputId){
+        idTv = inputId  ;
+        let check = false;
+        for(let i = 0; i < dataLbr.hBorrow.length; i++){
+            if(dataLbr.hBorrow[i].id === idTv){
+                check = true;
+                nameTv = dataLbr.hBorrow[i].name;
+                rl.question("Nhập ID sách mượn: ", function(inputIDS){
+                    idB = inputIDS;
+                    for(let j = 0; j < dataLbr.book.length; j++){
+                        if(dataLbr.book[j].id === idB){
+                            check = true;
+                            nameBook = dataLbr.book[j].name;
+                            let tempCountB = parseInt(dataLbr.book[i].count) - 1;
+                            let tempCountH = parseInt(dataLbr.hBorrow[j].total) + 1;
+                            //let stringnametemp = dataLbr.book[i].name;
+                            rl.question("Bạn có chắc: "
+                                + "\n========================================="
+                                + "\nHọc sinh: " + nameTv 
+                                + "\nMượn sách: " + nameBook
+                                + "\n========================================="
+                                + "\n1 => Đúng vậy."
+                                + "\n0 => Không đúng.", function(confirm){
+                                    let choose = parseInt(confirm);
+                                    switch(choose){
+                                        case 0:
+                                            HSHandle();
+                                            break;
+                                        case 1:
+                                            dataLbr.book[j].count = tempCountB.toString();
+                                            dataLbr.hBorrow[i].total = tempCountH.toString();
+                                            dataLbr.book[j].hBorrow.push({
+                                                "id": idTv,
+                                                "name": nameTv
+                                            });
+                                            dataLbr.hBorrow[i].bBorrow.push({
+                                                "id": idB,
+                                                "name": nameBook
+                                            })
+                                            SaveJson();
+                                            rl.question("Đã thêm vào danh sách, nhấn Enter để thoát!", function(quit){
+                                                HSHandle();
+                                            });
+                                            break;
+                                        default:
+                                            HSHandle();
+                                            break;
+                                    }
+                                }
+                            );
+                        }
+                    }
+                    if(!check){
+                        check = false;
+                        rl.question("Không có sách này! Nhấn Enter để thoát!!!!", function(confirm){
+                            HSHandle();
+                        })
+                    }
+                })
+            }
+        }
+        if(!check){
+            check = false;
+            rl.question("Không có học viên này! Nhấn Enter để thoát!!!!", function(confirm){
+                HSHandle();
+            })
+        }
+    })
+}
+
+function HSHandle(){
+let a;
+    console.log("=== Bạn Muốn Thao Tác Gì ? ====");
+    console.log("==============================");
+    console.log("1 => Thêm Thành Viên.");    
+    console.log("2 => Mượn Sách."); 
+    console.log("3 => Trả sách."); 
+    console.log("0 => Quay lại.");
+    EndLine();
+    rl.question("Bạn muốn làm gì?\n", function(input){
+        a = parseInt(input);
+        switch(a){
+            case 0:
+                Menu();
+                break;
+            case 1:
+                AddHS();
+                break;
+            case 2:
+                BorrowBook();
+                break;
+            case 3:
+                ReturnBook();
+                break;
+            default:
+                HSHandle();
+                break;
+        }
+    })
+}
 function Menu(){
     let a;
     for(let i = 0; i> dataLbr.book.length; i++){
@@ -260,11 +597,11 @@ function Menu(){
     console.log("=== Chào Mừng Bạn Đến Với Thư Viện ====");
     console.log("         Bạn Muốn Làm Gì ?             ");
     console.log("1 => Tra cứu.");    
-    console.log("2 => Tra cứu người mượn sách.");    
-    console.log("3 => Thêm sách vào thư viện.");    
-    console.log("4 => Thêm thông tin mượn sách.");    
+    console.log("2 => Thao tác với sách.");    
+    console.log("3 => Thao tác với học sinh.");    
+    console.log("0 => Thoát.");    
     EndLine();    
-    rl.question("Ban muon lam gi? \n", function(name){
+    rl.question("Bạn muốn làm gì? \n", function(name){
         a = parseInt(name);
         console.log(a);
         switch(a){
@@ -275,12 +612,14 @@ function Menu(){
                 CheckListMenu();
                 break;
             case 2:
-                rl.close();
+                BookHandle();
+                break;
+            case 3:
+                HSHandle();
                 break;
             default:
                 Menu();
                 break;
         }
     });
-
 }
